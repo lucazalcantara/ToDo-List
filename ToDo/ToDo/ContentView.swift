92e42925c08770025da19cbae8212a4e8bfbef44
Lucas Alcantara <lucasprestesmatos@hotmail.com>
2025-02-27 05:02:05 -0300

    Data Management - Projeto finalizado

--- ToDo/ToDo/ContentView.swift
+++ ToDo/ToDo/ContentView.swift
@@ -36,6 +36,24 @@ struct ContentView: View {
         todoItems.filter { $0.isCompleted }
     }
     
+    private func updateTodoItem(_ todoItem: TodoItem) {
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
+    }
+    
+    private func deleteTodoItem(_ todoItem: TodoItem) {
+        context.delete(todoItem)
+        
+        do {
+            try context.save()
+        } catch {
+            print(error)
+        }
+    }
+    
     var body: some View {
         VStack {
             TextField("Title", text: $title)
@@ -48,14 +66,32 @@ struct ContentView: View {
                 }
             List {
                 Section("Pending") {
-                    ForEach(pendingTodoItems) { todoItem in
-                        Text(todoItem.title ?? "")
+                    if pendingTodoItems.isEmpty {
+                        ContentUnavailableView("No items found.", systemImage: "doc")
+                    } else {
+                        ForEach(pendingTodoItems) { todoItem in
+                            TodoCellView(todoItem: todoItem, onChanged: updateTodoItem)
+                        }.onDelete(perform: { indexSet in
+                            indexSet.forEach { indexSet in
+                                let todoItem = pendingTodoItems[indexSet]
+                                deleteTodoItem(todoItem)
+                            }
+                        })
                     }
                 }
                 
                 Section("Completed") {
-                    ForEach(completedTodoItems) { todoItem in
-                        Text(todoItem.title ?? "")
+                    if completedTodoItems.isEmpty {
+                        ContentUnavailableView("No items found.", systemImage: "doc")
+                    } else {
+                        ForEach(completedTodoItems) { todoItem in
+                            TodoCellView(todoItem: todoItem, onChanged: updateTodoItem)
+                        }.onDelete(perform: { indexSet in
+                            indexSet.forEach { indexSet in
+                                let todoItem = completedTodoItems[indexSet]
+                                deleteTodoItem(todoItem)
+                            }
+                        })
                     }
                 }
             }.listStyle(.plain)
@@ -64,6 +100,33 @@ struct ContentView: View {
         }
         .padding()
         .navigationTitle("To-Do List")
+    }
+}
+
+struct TodoCellView: View {
+    
+    let todoItem: TodoItem
+    let onChanged: (TodoItem) -> Void
+    
+    var body: some View {
+        HStack {
+            Image(systemName: todoItem.isCompleted ? "checkmark.square" : "square")
+                .onTapGesture {
+                    todoItem.isCompleted = !todoItem.isCompleted
+                    onChanged(todoItem)
+                }
+            if todoItem.isCompleted {
+                Text(todoItem.title ?? "")
+            } else {
+                TextField("", text: Binding(get: {
+                    todoItem.title ?? ""
+                }, set: { newValue in
+                    todoItem.title = newValue
+                })).onSubmit {
+                    onChanged(todoItem)
+                }
+            }
+        }
     }
 }
 
